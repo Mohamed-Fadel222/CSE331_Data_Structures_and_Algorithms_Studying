24f64665f704ec59478c056944546e0c11c91757
Mohamed-Fadel222 <116431641+Mohamed-Fadel222@users.noreply.github.com>
2023-10-07 20:41:32 +0300

    kk

---
+++ sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int sort(int arr[],int arr_size){
+
+    for (int i=0;i<arr_size;i++){
+        if (arr[i]>arr[i+1]){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i]=temp;
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+    return *arr;
+}
+
+int main(){
+
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int arr_size=sizeof(arr)/sizeof(arr[0]);
+    sort(arr,arr_size);
+    for (int i=0;i<arr_size;i++){
+        cout<<arr[i]<<endl;
+    }
+
+    return 0;
+
+
+
+
+
+
+}
